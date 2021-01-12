d1084f247bd9416b329a74cb4f3857d2db4bc691
al-mint <devops.sune@gmail.com>
2021-01-12 13:36:51 +0800

    Add author's page and adjust the author of first post

--- assets/js/lunr/lunr-store.js
+++ assets/js/lunr/lunr-store.js
@@ -1,4 +1,34 @@
 var store = [{
+        "title": "Al Gray",
+        "excerpt":"Allan is a geologist and electrical geophysicist who moved to the Philippines with a simple idea: this is the best country in the world for developing solar electric transportation systems.  ","categories": [],
+        "tags": [],
+        "url": "https://sunetrike.bitbucket.io/team/al-gray",
+        "teaser": "https://sunetrike.bitbucket.io/assets/images/sunev.JPG"
+      },{
+        "title": "Clayton Gray",
+        "excerpt":"Clayton is a serial &amp; social technopreneur and change agent, with over four decades of experience spanning agribusiness, construction, wireless internet delivery, financial services, and renewable energy industries. Currently, Clayton serves as the co-founder and CEO of Alset Integrated Energy Hong Kong Limited. Alset is the international arm of sunE,...","categories": [],
+        "tags": [],
+        "url": "https://sunetrike.bitbucket.io/team/clayton-gray",
+        "teaser": "https://sunetrike.bitbucket.io/assets/images/sunev.JPG"
+      },{
+        "title": "Kwadwo Ansah Antwi",
+        "excerpt":"Dr. Kwadwo Konadu Ansah-Antwi is currently CEO and co-Founder of Hessner Technologies Limited and Dyna Insights both of which are Hong based companies. He earned his Bachelor of Materials Science and Engineering at the University of Ghana, in 2009, and his PhD in Electrical and Computer Engineering from the National...","categories": [],
+        "tags": [],
+        "url": "https://sunetrike.bitbucket.io/team/kwadwo-ansah-antwi",
+        "teaser": "https://sunetrike.bitbucket.io/assets/images/sunev.JPG"
+      },{
+        "title": "Lois Gray",
+        "excerpt":"Ms. Gray (BSA, MBA) is a Certified Management Consultant with over three decades of experience providing business advisory services to firms and organizations across a range of sectors. She brings a broad outlook and a diverse skill set to her work. This eclectic background was acquired through her work in...","categories": [],
+        "tags": [],
+        "url": "https://sunetrike.bitbucket.io/team/lois-gray",
+        "teaser": "https://sunetrike.bitbucket.io/assets/images/sunev.JPG"
+      },{
+        "title": "Phil Gray",
+        "excerpt":"Co-founder &amp; General Manager of sunE in Batangas operation over the past 4 years in Santo Tomas Batangas. In that business Phil was responsible for the marketing human resources and cash management of the service. The shuttle operations ran 18 hours a day every day for 3 and half years...","categories": [],
+        "tags": [],
+        "url": "https://sunetrike.bitbucket.io/team/phil-gray",
+        "teaser": "https://sunetrike.bitbucket.io/assets/images/sunev.JPG"
+      },{
         "title": "sunE is now a Solar Impulse Efficient Solution",
         "excerpt":"sunE has been named by the Solar Impulse World Alliance as one of their 1000 efficient solutions. As part of the Solar Impulse World Alliance of Efficient Solutions, sunE will showcase our solution to innovators, investors and decision makers around the world. The official announcement will happen by solar impulse...","categories": ["jekyll","update"],
         "tags": [],
