ae2925d8225888f312af9d5eb3b83c0f4dbf907d
al-mint <devops.sune@gmail.com>
2021-01-19 10:03:17 +0800

    adjust domain in config

--- assets/js/lunr/lunr-store.js
+++ assets/js/lunr/lunr-store.js
@@ -2,48 +2,48 @@ var store = [{
         "title": "Al Gray",
         "excerpt":"Allan is a geologist and electrical geophysicist who moved to the Philippines with a simple idea: this is the best country in the world for developing solar electric transportation systems.  ","categories": [],
         "tags": [],
-        "url": "https://sunetrike.bitbucket.io/team/al-gray",
-        "teaser": "https://sunetrike.bitbucket.io/assets/images/sunev.JPG"
+        "url": "https://sunetrike.github.io/team/al-gray",
+        "teaser": "https://sunetrike.github.io/assets/images/sunev.JPG"
       },{
         "title": "Clayton Gray",
         "excerpt":"Clayton is a serial &amp; social technopreneur and change agent, with over four decades of experience spanning agribusiness, construction, wireless internet delivery, financial services, and renewable energy industries. Currently, Clayton serves as the co-founder and CEO of Alset Integrated Energy Hong Kong Limited. Alset is the international arm of sunE,...","categories": [],
         "tags": [],
-        "url": "https://sunetrike.bitbucket.io/team/clayton-gray",
-        "teaser": "https://sunetrike.bitbucket.io/assets/images/sunev.JPG"
+        "url": "https://sunetrike.github.io/team/clayton-gray",
+        "teaser": "https://sunetrike.github.io/assets/images/sunev.JPG"
       },{
         "title": "Kwadwo Ansah Antwi",
         "excerpt":"Dr. Kwadwo Konadu Ansah-Antwi is currently CEO and co-Founder of Hessner Technologies Limited and Dyna Insights both of which are Hong based companies. He earned his Bachelor of Materials Science and Engineering at the University of Ghana, in 2009, and his PhD in Electrical and Computer Engineering from the National...","categories": [],
         "tags": [],
-        "url": "https://sunetrike.bitbucket.io/team/kwadwo-ansah-antwi",
-        "teaser": "https://sunetrike.bitbucket.io/assets/images/sunev.JPG"
+        "url": "https://sunetrike.github.io/team/kwadwo-ansah-antwi",
+        "teaser": "https://sunetrike.github.io/assets/images/sunev.JPG"
       },{
         "title": "Lois Gray",
         "excerpt":"Ms. Gray (BSA, MBA) is a Certified Management Consultant with over three decades of experience providing business advisory services to firms and organizations across a range of sectors. She brings a broad outlook and a diverse skill set to her work. This eclectic background was acquired through her work in...","categories": [],
         "tags": [],
-        "url": "https://sunetrike.bitbucket.io/team/lois-gray",
-        "teaser": "https://sunetrike.bitbucket.io/assets/images/sunev.JPG"
+        "url": "https://sunetrike.github.io/team/lois-gray",
+        "teaser": "https://sunetrike.github.io/assets/images/sunev.JPG"
       },{
         "title": "Phil Gray",
         "excerpt":"Co-founder &amp; General Manager of sunE in Batangas operation over the past 4 years in Santo Tomas Batangas. In that business Phil was responsible for the marketing human resources and cash management of the service. The shuttle operations ran 18 hours a day every day for 3 and half years...","categories": [],
         "tags": [],
-        "url": "https://sunetrike.bitbucket.io/team/phil-gray",
-        "teaser": "https://sunetrike.bitbucket.io/assets/images/sunev.JPG"
+        "url": "https://sunetrike.github.io/team/phil-gray",
+        "teaser": "https://sunetrike.github.io/assets/images/sunev.JPG"
       },{
         "title": "sunE is now a Solar Impulse Efficient Solution",
         "excerpt":"sunE has been named by the Solar Impulse World Alliance as one of their 1000 efficient solutions. As part of the Solar Impulse World Alliance of Efficient Solutions, sunE will showcase our solution to innovators, investors and decision makers around the world. The official announcement will happen by solar impulse...","categories": ["solar-impulse"],
         "tags": [],
-        "url": "https://sunetrike.bitbucket.io/solar-impulse/sune-is-now-a-solar-impulse-label/",
-        "teaser": "https://sunetrike.bitbucket.io/assets/images/SIF_LABEL.png"
+        "url": "https://sunetrike.github.io/solar-impulse/sune-is-now-a-solar-impulse-label/",
+        "teaser": "https://sunetrike.github.io/assets/images/SIF_LABEL.png"
       },{
         "title": "Supercapacitors for LSEVs",
         "excerpt":"Supercapacitors increase battery efficiency by protecting the battery and other system components from excessive stress, and they enable onboard energy capture to reduce the energy cost for vehicle operation. Combining supercapacitors, batteries and onboard energy harvesting improves overall system operation efficiency and allows the vehicle to travel further on a...","categories": ["supercapacitors"],
         "tags": [],
-        "url": "https://sunetrike.bitbucket.io/supercapacitors/supercapacitors-for-lsevs/",
-        "teaser": "https://sunetrike.bitbucket.io/assets/images/supercapacitors/lightning.png"
+        "url": "https://sunetrike.github.io/supercapacitors/supercapacitors-for-lsevs/",
+        "teaser": "https://sunetrike.github.io/assets/images/supercapacitors/lightning.png"
       },{
         "title": "My First Lung Fractal",
         "excerpt":"Since I first moved to the Philippines, the air pollution has been one of the hardest features to come to grips with. I seem to have especially sensitive lungs as I come down with lung infections 2 to 3 times a year and it lingers for several weeks meanwhile nobody...","categories": ["fractals","lungs","dr-geo"],
         "tags": [],
-        "url": "https://sunetrike.bitbucket.io/fractals/lungs/dr-geo/first-lung-fractal/",
-        "teaser": "https://sunetrike.bitbucket.io/assets/images/fractals/lung-fractal-1.png"
+        "url": "https://sunetrike.github.io/fractals/lungs/dr-geo/first-lung-fractal/",
+        "teaser": "https://sunetrike.github.io/assets/images/fractals/lung-fractal-1.png"
       }]
