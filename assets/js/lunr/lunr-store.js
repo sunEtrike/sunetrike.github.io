160545ab593f81f8f6e00fcfc649623331b53d50
graial <graial@deb-al.sunEtrike>
2024-05-02 16:45:39 +0800

    upgrade ruby & jekyll

--- assets/js/lunr/lunr-store.js
+++ assets/js/lunr/lunr-store.js
@@ -2,126 +2,126 @@ var store = [{
         "title": "Al Gray",
         "excerpt":"Allan is a geologist and electrical geophysicist who moved to the Philippines with a simple idea: this is the best country in the world for developing solar electric transportation systems. You can find him about an hour south of Manila between the dinosaur footprint and the lake that exploded in...","categories": [],
         "tags": [],
-        "url": "https://sunetrike.com/team/al-gray",
-        "teaser": "https://sunetrike.com/assets/images/sunev-thumb.JPG"
+        "url": "/team/al-gray",
+        "teaser": "/assets/images/sunev-thumb.JPG"
       },{
         "title": "Clayton Gray",
         "excerpt":"Clayton is a serial &amp; social technopreneur and change agent, with over four decades of experience spanning agribusiness, construction, wireless internet delivery, financial services, and renewable energy industries. Currently, Clayton serves as the co-founder and CEO of Alset Integrated Energy Hong Kong Limited. Alset is the international arm of sunE,...","categories": [],
         "tags": [],
-        "url": "https://sunetrike.com/team/clayton-gray",
-        "teaser": "https://sunetrike.com/assets/images/sunev-thumb.JPG"
+        "url": "/team/clayton-gray",
+        "teaser": "/assets/images/sunev-thumb.JPG"
       },{
         "title": "Lois Gray",
         "excerpt":"Ms. Gray (BSA, MBA) is a Certified Management Consultant with over three decades of experience providing business advisory services to firms and organizations across a range of sectors. She brings a broad outlook and a diverse skill set to her work. This eclectic background was acquired through her work in...","categories": [],
         "tags": [],
-        "url": "https://sunetrike.com/team/lois-gray",
-        "teaser": "https://sunetrike.com/assets/images/sunev-thumb.JPG"
+        "url": "/team/lois-gray",
+        "teaser": "/assets/images/sunev-thumb.JPG"
       },{
         "title": "Phil Gray",
         "excerpt":"Co-founder &amp; General Manager of sunE in Batangas operation over the past 4 years in Santo Tomas Batangas. In that business Phil was responsible for the marketing human resources and cash management of the service. The shuttle operations ran 18 hours a day every day for 3 and half years...","categories": [],
         "tags": [],
-        "url": "https://sunetrike.com/team/phil-gray",
-        "teaser": "https://sunetrike.com/assets/images/sunev-thumb.JPG"
+        "url": "/team/phil-gray",
+        "teaser": "/assets/images/sunev-thumb.JPG"
       },{
         "title": "Absorbent Roadway – Concept outline",
         "excerpt":"Thank you to the R&amp;D team of LafargeHolcim for making this draft concept paper available to us for our submission to the 2018 Smart Infrastructure Challenge The absorbent roadway seeks to absorb and disperse localized flooding caused by storm surges. Continuous accumulation of plastic is resulting in two adverse consequences:...","categories": ["smart-infrastructure-challenge"],
         "tags": [],
-        "url": "https://sunetrike.com/smart-infrastructure-challenge/absorbent-roadway-concept/",
-        "teaser": "https://sunetrike.com/assets/images/smart-infrastructure-challenge/sponge-city-info.jpeg"
+        "url": "/smart-infrastructure-challenge/absorbent-roadway-concept/",
+        "teaser": "/assets/images/smart-infrastructure-challenge/sponge-city-info.jpeg"
       },{
         "title": "Bringing Vitamins and Solar Lamps to Bansud, Philippines!",
         "excerpt":"As a joint project between two Rotary districts: 3820 Calabarzon &amp; 3830 Makati. Around 20 Rotarians visited the Mangyan tribe in the Bansud Region of Mindoro. The project is called Community Lighting and Iba Pa (CLAP) and it included donations of medical supplies, clothing, solar-lanterns, school-supplies. Allan Gray from sunEtrike...","categories": ["solar-coin","rotary"],
         "tags": [],
-        "url": "https://sunetrike.com/solar-coin/rotary/vitamins-and-solar-to-bansud/",
-        "teaser": "https://sunetrike.com/assets/images/rotary/CLAP-1-768x576.jpeg"
+        "url": "/solar-coin/rotary/vitamins-and-solar-to-bansud/",
+        "teaser": "/assets/images/rotary/CLAP-1-768x576.jpeg"
       },{
         "title": "Employment of Persons with Disabilities (PWDs) in the Philippines – Study",
         "excerpt":"As part of our submissions for the 2018 Smart Infrastructure Challenge, our team has chosen to include mobility access for PWDs into the scope of our portfolio of projects. In addition to the disability access of newly emerging vehicles within the PUV modernization program, we are also proposing a low-cost...","categories": ["smart-infrastructure-challenge"],
         "tags": [],
-        "url": "https://sunetrike.com/smart-infrastructure-challenge/employment-of-pwds/",
-        "teaser": "https://sunetrike.com/assets/images/smart-infrastructure-challenge/pwd.jpeg"
+        "url": "/smart-infrastructure-challenge/employment-of-pwds/",
+        "teaser": "/assets/images/smart-infrastructure-challenge/pwd.jpeg"
       },{
         "title": "Tales from Trying to Improve Transportation in the Philippines - Presentation for CUNY's Administration of Global Institutions class",
         "excerpt":"As a follow-on to our participation in the 2018 Smart Infrastructure Challenge, I got the very cool opportunity to share some of my experiences with Professor Binda’s class on Administration of Global Institutions at the City University of New York (CUNY). They were reviewing the ASEAN zone and so I...","categories": ["presentations","tranportation"],
         "tags": [],
-        "url": "https://sunetrike.com/presentations/tranportation/cuny-global-institutions-presentation-tales-of-trying-to-improve-transportation-in-the-philippines/",
-        "teaser": "https://sunetrike.com/assets/images/banners/transport-tales-banner.png"
+        "url": "/presentations/tranportation/cuny-global-institutions-presentation-tales-of-trying-to-improve-transportation-in-the-philippines/",
+        "teaser": "/assets/images/banners/transport-tales-banner.png"
       },{
         "title": "sunE is now a Solar Impulse Efficient Solution",
         "excerpt":"sunE has been named by the Solar Impulse World Alliance as one of their 1000 efficient solutions. As part of the Solar Impulse World Alliance of Efficient Solutions, sunE will showcase our solution to innovators, investors and decision makers around the world. The official announcement will happen by solar impulse...","categories": ["solar-impulse"],
         "tags": [],
-        "url": "https://sunetrike.com/solar-impulse/sune-is-now-a-solar-impulse-label/",
-        "teaser": "https://sunetrike.com/assets/images/SIF_LABEL.png"
+        "url": "/solar-impulse/sune-is-now-a-solar-impulse-label/",
+        "teaser": "/assets/images/SIF_LABEL.png"
       },{
         "title": "The role of technology in bringing us to a Carbon-neutral Future - Presentation for CUNY's Administration of Global Institutions class",
         "excerpt":"As the new school year came along, I got the chance to come in for another of Professor Binda’s classes on Administration of Global Institutions at the City University of New York (CUNY). Once again they were discussing the ASEAN zone but our exciting news this time was getting accepted...","categories": ["presentations","carbon-neutral"],
         "tags": [],
-        "url": "https://sunetrike.com/presentations/carbon-neutral/cuny-global-institutions-presentation-he-role-of-technology-in-bringing-us-to-a-carbon-neutral-future/",
-        "teaser": "https://sunetrike.com/assets/images/banners/wright-brothers.jpeg"
+        "url": "/presentations/carbon-neutral/cuny-global-institutions-presentation-he-role-of-technology-in-bringing-us-to-a-carbon-neutral-future/",
+        "teaser": "/assets/images/banners/wright-brothers.jpeg"
       },{
         "title": "Findings from the 2019 IEEE Smart City Conference in Casablanca, Morocco",
         "excerpt":"Fresh off an exciting trip to Casablanca, Morocco for the IEEE’s 2019 Smart Cities Conference (Oct 2019), I was keen to apply my learnings and get rolling with plans for automated traffic data collection in small to mid-sized municipalities across Southern Luzon as per the plans laid out by Smart...","categories": ["presentations","smart-cities"],
         "tags": [],
-        "url": "https://sunetrike.com/presentations/smart-cities/BCBP-Canlubang-presentation-on-Smart-City-technologies/",
-        "teaser": "https://sunetrike.com/assets/images/smart-infrastructure-challenge/BCBP-flyer.jpg"
+        "url": "/presentations/smart-cities/BCBP-Canlubang-presentation-on-Smart-City-technologies/",
+        "teaser": "/assets/images/smart-infrastructure-challenge/BCBP-flyer.jpg"
       },{
         "title": "Supercapacitors for LSEVs",
         "excerpt":"Supercapacitors increase battery efficiency by protecting the battery and other system components from excessive stress, and they enable onboard energy capture to reduce the energy cost for vehicle operation. Combining supercapacitors, batteries and onboard energy harvesting improves overall system operation efficiency and allows the vehicle to travel further on a...","categories": ["supercapacitors"],
         "tags": [],
-        "url": "https://sunetrike.com/supercapacitors/supercapacitors-for-lsevs/",
-        "teaser": "https://sunetrike.com/assets/images/supercapacitors/lightning.png"
+        "url": "/supercapacitors/supercapacitors-for-lsevs/",
+        "teaser": "/assets/images/supercapacitors/lightning.png"
       },{
         "title": "My First Lung Fractal",
         "excerpt":"Since I first moved to the Philippines, the air pollution has been one of the hardest features to come to grips with. I seem to have especially sensitive lungs as I come down with lung infections 2 to 3 times a year and it lingers for several weeks meanwhile nobody...","categories": ["fractals","lungs","dr-geo"],
         "tags": [],
-        "url": "https://sunetrike.com/fractals/lungs/dr-geo/first-lung-fractal/",
-        "teaser": "https://sunetrike.com/assets/images/fractals/lung-fractal-1.png"
+        "url": "/fractals/lungs/dr-geo/first-lung-fractal/",
+        "teaser": "/assets/images/fractals/lung-fractal-1.png"
       },{
         "title": "My Pedal Powered Adventure - An hommage to Piccard's Epic Voyage",
         "excerpt":"In a salute to the 1000 solution milestone of the Solar Impulse Foundation, This story is a bit of an hommage to the immensely inspiring 2010 TED talk by Captain Bertrand Piccard titled ‘My Solar-Powered Adventure’. This entrepreneurship adventure of ours has had many highs and lows. His speech has...","categories": ["solar-impulse"],
         "tags": [],
-        "url": "https://sunetrike.com/solar-impulse/my-pedal-powered-journey/",
-        "teaser": "https://sunetrike.com/assets/images/pedal-power/three_buttes.jpg"
+        "url": "/solar-impulse/my-pedal-powered-journey/",
+        "teaser": "/assets/images/pedal-power/three_buttes.jpg"
       },{
         "title": "SE Asia's Labelled Solutions for water cleanup.",
         "excerpt":"Here we are, one thousand solutions closer to a cleaner planet. With the #beyond1000solutions campaign, Solar Impulse is gearing up to bring the forward the next wave of Cleantech into the world. To get the ball rolling for the next phase, I’ve put together a short list of some of...","categories": ["solar-impulse"],
         "tags": [],
-        "url": "https://sunetrike.com/solar-impulse/se-asias-water-cleanup-solutions/",
-        "teaser": "https://sunetrike.com/assets/images/water-cleanup/hero.jpeg"
+        "url": "/solar-impulse/se-asias-water-cleanup-solutions/",
+        "teaser": "/assets/images/water-cleanup/hero.jpeg"
       },{
         "title": "Past, Present and Future of Environmentalism - Presentation for Mapua's Social Sciences and Education week",
         "excerpt":"As part the week in Social Sciences and Education at Mapua University, I was grateful for the opportunity to share my thoughts about environmentalism with some of the bright, emerging minds of the Philippines. It appears as though your browser doesn't have a PDF plugin. It is available for Download...","categories": ["presentations","environmentalism"],
         "tags": [],
-        "url": "https://sunetrike.com/presentations/environmentalism/mapua-humanities-presentation-environmentalism-past-present-future/",
-        "teaser": "https://sunetrike.com/assets/images/mapua/mapual-background.jpg"
+        "url": "/presentations/environmentalism/mapua-humanities-presentation-environmentalism-past-present-future/",
+        "teaser": "/assets/images/mapua/mapual-background.jpg"
       },{
         "title": "sunE declares intention to achieve Net-Zero across its supply chain by 2025.",
         "excerpt":"We are pleased to announce a minor (but important) change in compass-heading for the sunE Venture.  Please see our declaration to achieve net-zero here      Interested in hearing more from us?  Check out our newsletter   ","categories": ["announcements"],
         "tags": [],
-        "url": "https://sunetrike.com/announcements/declaration-to-achieve-net-zero/",
-        "teaser": "https://sunetrike.com/assets/images/sunev-thumb.JPG"
+        "url": "/announcements/declaration-to-achieve-net-zero/",
+        "teaser": "/assets/images/sunev-thumb.JPG"
       },{
         "title": "In flight, I attain a higher consciousness.  Fear disappears - Translation of Bertrand Piccard's interview on La Croix",
         "excerpt":"In April, Capt Piccard sat down with the Malo Tresca and the podcast Place des Religions for a discussion about exploration, consciousness and how the Solar Impulse Network of 1000 Solutions can contribute to the great existential challenge of the 21st century. I decided to do a translation into English....","categories": ["solar-impulse"],
         "tags": [],
-        "url": "https://sunetrike.com/solar-impulse/in-flight-i-attain-a-higher-consciousness/",
-        "teaser": "https://sunetrike.com/assets/images/solar-impulse-sun-thumb.jpeg"
+        "url": "/solar-impulse/in-flight-i-attain-a-higher-consciousness/",
+        "teaser": "/assets/images/solar-impulse-sun-thumb.jpeg"
       },{
         "title": "A TDD rake based build-system for multi-targeting Arm Cortex-M - using the teensy 3.6 as an example",
         "excerpt":"Learn how to create modular code for embedded systems using a Test Driven Development approach Intro and background Want to dig deeper into developing with Arm Cortex MCUs? Interested in seeing how embedded systems are tested or developing some TDD skills for Arduino projects? Have a go at this tutorial!...","categories": ["open-source","embedded","teensy"],
         "tags": [],
-        "url": "https://sunetrike.com/open-source/embedded/teensy/a-test-drive-build-system-for-teensy/",
-        "teaser": "https://sunetrike.com/assets/images/teensy_rake/teensy.png"
+        "url": "/open-source/embedded/teensy/a-test-drive-build-system-for-teensy/",
+        "teaser": "/assets/images/teensy_rake/teensy.png"
       },{
         "title": "Press Release - International Research Collaboration for energy harvesting vehicle suspension",
         "excerpt":"sunE is pleased to announce the undertaking of a new international research collaboration around energy harvesting suspencsion systems for low-speed electric vehicles. It appears as though your browser doesn't have a PDF plugin. It is available for Download here: Nov 2021 Energy Harvester project press release Interested in hearing more...","categories": ["energy-harvester","announcements"],
         "tags": [],
-        "url": "https://sunetrike.com/energy-harvester/announcements/sune-energy-harvester-research-grant/",
-        "teaser": "https://sunetrike.com/assets/images/banners/harvester-pr.png"
+        "url": "/energy-harvester/announcements/sune-energy-harvester-research-grant/",
+        "teaser": "/assets/images/banners/harvester-pr.png"
       },{
         "title": "sunE sits down with the Sustainability/Smart Cities Podcast!",
         "excerpt":"Allan Gray was given the chance to chat with Jim Frazier on the Sustainbaility (FKA Smart Cities) podcast about sunE and our work in the Filipino public transportation sector. Check it out below! You can also check out the other episodes from the podcast here Interested in hearing more from...","categories": ["smart-cities","transportation"],
         "tags": [],
-        "url": "https://sunetrike.com/smart-cities/transportation/sune-on-the-smart-cities-podcast/",
-        "teaser": "https://sunetrike.com/assets/images/arc-podcast-logo.jpg"
+        "url": "/smart-cities/transportation/sune-on-the-smart-cities-podcast/",
+        "teaser": "/assets/images/arc-podcast-logo.jpg"
       }]
